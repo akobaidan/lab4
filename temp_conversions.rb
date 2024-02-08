60871313c69adae7dbba3662a3fc7b1d3f8ba30b
akobaidan <akobaida@andrew.cmu.edu>
2024-02-08 12:22:54 +0300

    revised

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,8 +1,12 @@
 def convert(temp)
-    cel = (temp-32)*5.0 / 9.0
-    return cel
+    return "Temperature must be an integer" if temp.class != Integer
+    return "Temperature below Absolute Zero" if temp < -474 
+    (temp-32)*5.0 / 9.0
 end
 
 puts convert(32)          
 puts convert(50)          
 puts convert(212)
+
+puts convert("zero")
+puts convert(-500)
